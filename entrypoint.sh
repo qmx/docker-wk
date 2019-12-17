a5dee6a9fe37bef1d0f7e277619f6fd71f58c6c4
Douglas Campos <qmx@qmx.me>
2019-12-17 04:29:45 +0000

    fix docker socket permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# fix docker socket perms
+if [ -S "/var/run/docker.sock" ]; then
+       SOCK_GID="$(stat -c "%g" /var/run/docker.sock)"
+       CURR_GID="$(getent group docker|cut -d: -f3)"
+       if [ "$SOCK_GID" != "$CURR_GID" ]; then
+               echo "fixing docker gid"
+               groupmod -g "${SOCK_GID}" docker
+       fi
+fi
+
 HOSTNAME="$(hostname)"
 echo "127.0.0.1 $HOSTNAME" >> /etc/hosts
 
